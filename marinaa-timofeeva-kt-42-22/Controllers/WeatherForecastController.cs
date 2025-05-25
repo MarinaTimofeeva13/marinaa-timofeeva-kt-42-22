fd0961f8ee171db08cff353294e521326c7b3a32
marinaa-timofeeva-kt-42-22 <marina.timofeeva1316@gmail.com>
2025-05-26 00:24:07 +0300

    add logging and WeatherForecast test request

--- marinaa-timofeeva-kt-42-22/Controllers/WeatherForecastController.cs
+++ marinaa-timofeeva-kt-42-22/Controllers/WeatherForecastController.cs
@@ -18,9 +18,13 @@ namespace marinaa_timofeeva_kt_42_22.Controllers
             _logger = logger;
         }
 
+
+
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            _logger.LogError("Method was called");
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
@@ -29,5 +33,14 @@ namespace marinaa_timofeeva_kt_42_22.Controllers
             })
             .ToArray();
         }
+            [HttpPost(Name = "AddNewSummary")]
+        public string[] AddNewSummary(string newSummary)
+        {
+            _logger.LogError("New method was called");
+            var list = Summaries.ToList();
+            list.Add(newSummary);
+            return list.ToArray();
+        }
+
     }
 }
